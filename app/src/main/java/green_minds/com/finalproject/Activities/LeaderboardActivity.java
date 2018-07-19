5340e167baa0ae4c955b0798d43d5052208885a2
Cicely Beckford <cicelybeckford@fb.com>
2018-07-19 10:25:18 -0700

    -Added user persistence

--- app/src/main/java/green_minds/com/finalproject/Activities/LeaderboardActivity.java
+++ app/src/main/java/green_minds/com/finalproject/Activities/LeaderboardActivity.java
@@ -1,15 +1,62 @@
 package green_minds.com.finalproject.Activities;
 
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import green_minds.com.finalproject.Adapters.LeaderboardAdapter;
 import green_minds.com.finalproject.R;
 
 public class LeaderboardActivity extends AppCompatActivity {
+    @BindView(R.id.swipe_container) public SwipeRefreshLayout swipeContainer;
+    @BindView(R.id.rvUsers) public RecyclerView rvUsers;
+    private ParseUser newUser;
+    protected LeaderboardAdapter leaderboardAdapter;
+    protected ArrayList<ParseUser> users;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leaderboard);
+        ButterKnife.bind(this);
+
+        users = new ArrayList<>();
+        leaderboardAdapter = new LeaderboardAdapter(users);
+        rvUsers.setLayoutManager(new LinearLayoutManager(this));
+        rvUsers.setAdapter(leaderboardAdapter);
+
+        showLeaderboard();
+    }
+
+    private void showLeaderboard() {
+        ParseQuery query = ParseUser.getQuery();
+        query.setLimit(20).findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> objects, ParseException e) {
+                ParseUser user = null;
+                if (e == null) {
+                    for (int i = objects.size() - 1; i >= 0; i--) {
+                        user = objects.get(i);
+                        users.add(user);
+                        leaderboardAdapter.notifyItemChanged(users.size() - 1);
+                    }
+                }
+                else {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
--- app/src/main/java/green_minds/com/finalproject/Adapters/LeaderboardAdapter.java
+++ app/src/main/java/green_minds/com/finalproject/Adapters/LeaderboardAdapter.java
@@ -1,4 +1,70 @@
 package green_minds.com.finalproject.Adapters;
 
-public class LeaderboardAdapter {
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import green_minds.com.finalproject.R;
+
+public class LeaderboardAdapter extends RecyclerView.Adapter<LeaderboardAdapter.ViewHolder> {
+
+    private List<ParseUser> mUsers;
+    Context context;
+
+    public LeaderboardAdapter(List<ParseUser> users) {mUsers = users;}
+
+    public void addAll (ArrayList<ParseUser> list) {
+        mUsers.addAll(list);
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View leaderboardView = inflater.inflate(R.layout.item_leaderboard, parent, false);
+        ViewHolder viewHolder = new ViewHolder(leaderboardView);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        ParseUser user = mUsers.get(position);
+        Integer userPts = user.getInt("checkincount") + user.getInt("pincount");
+        holder.tvPosition.setText(String.valueOf(position + 1));
+        holder.tvUserLeaderbaord.setText(user.getUsername());
+        holder.tvPts.setText(String.valueOf(userPts));
+
+        //Glide.with(context).load(user.getParseFile("image").getUrl()).into(holder.ivUserImg);
+    }
+
+    @Override
+    public int getItemCount() { return mUsers.size();}
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.tvPostion) TextView tvPosition;
+        @BindView(R.id.ivUserImg) ImageView ivUserImg;
+        @BindView(R.id.tvUserLeaderboard) TextView tvUserLeaderbaord;
+        @BindView(R.id.tvPts) TextView tvPts;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
 }
