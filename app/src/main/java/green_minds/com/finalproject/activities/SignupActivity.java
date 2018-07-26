c46be1a4f5487747bd227a55ee5baede61ccda1c
Cicely Beckford <cicelybeckford@fb.com>
2018-07-25 18:34:08 -0700

    -Completed sign up with Facebook

--- app/src/main/java/green_minds/com/finalproject/activities/SignupActivity.java
+++ app/src/main/java/green_minds/com/finalproject/activities/SignupActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
@@ -14,12 +15,22 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
+import com.facebook.login.widget.LoginButton;
 import com.parse.FindCallback;
+import com.parse.LogInCallback;
 import com.parse.ParseException;
+import com.parse.ParseFacebookUtils;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.SignUpCallback;
 
+import org.json.JSONException;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
@@ -28,23 +39,33 @@ import green_minds.com.finalproject.R;
 import green_minds.com.finalproject.adapters.SchoolAutoCompleteAdapter;
 import green_minds.com.finalproject.model.DelayAutoCompleteTextView;
 
+
 public class SignupActivity extends AppCompatActivity {
 
     //public final static int RESULT_LOAD_IMAGE = 1;
     @BindView(R.id.etUsernameInput) public EditText etUsernameInput;
     @BindView(R.id.etPasswordInput) public EditText etPasswordInput;
+    @BindView(R.id.etEmailInput) public EditText etEmailInput;
     @BindView(R.id.btnSignup) public Button btnSignup;
     @BindView(R.id.rgSelection) public RadioGroup rgSelection;
     @BindView(R.id.rbWork) public RadioButton rbWork;
     @BindView(R.id.etCompany) public EditText etCompany;
     @BindView(R.id.tvUsernameTaken) public TextView tvUsernameTaken;
     @BindView(R.id.atvSchoolName) public DelayAutoCompleteTextView atvSchoolName;
+    @BindView(R.id.login_button) public LoginButton fbloginButton;
+    public static final List<String> mPermissions = new ArrayList<String>() {{ add("email"); }};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
         ButterKnife.bind(this);
+        ParseFacebookUtils.initialize(this);
+
+//        if (ParseUser.getCurrentUser() != null) {
+//            startActivity(new Intent(this, MapActivity.class));
+//            finish();
+//        }
 
         atvSchoolName.setThreshold(2);
         atvSchoolName.setAdapter(new SchoolAutoCompleteAdapter(this));
@@ -88,10 +109,11 @@ public class SignupActivity extends AppCompatActivity {
                                 String username = etUsernameInput.getText().toString();
                                 String password = etPasswordInput.getText().toString();
                                 String connection = atvSchoolName.getText().toString();
+                                String email = etEmailInput.getText().toString();
                                 if (rbWork.isChecked()) connection = etCompany.getText().toString();
                                 btnSignup.setEnabled(false);
                                 Toast.makeText(getApplicationContext(),"Creating user", Toast.LENGTH_SHORT).show();
-                                signUp (username, password, connection);
+                                signUp (username, password, connection, email);
                             } else {
                                 tvUsernameTaken.setText("Username already exists.");
                                 tvUsernameTaken.setVisibility(View.VISIBLE);
@@ -104,18 +126,92 @@ public class SignupActivity extends AppCompatActivity {
                 });
             }
         });
+
+        fbloginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParseFacebookUtils.logInWithReadPermissionsInBackground(SignupActivity.this, mPermissions, new LogInCallback() {
+                    @Override
+                    public void done(ParseUser user, ParseException e) {
+                        if (user == null) {
+                            Log.d("MyApp", "Uh oh. The user cancelled the Facebook login.");
+                        } else if (user.isNew()) {
+                            Log.d("MyApp", "User signed up and logged in through Facebook!");
+                            getUserDetailFromFB();
+                        } else {
+                            Log.d("MyApp", "User logged in through Facebook!");
+                            getUserDetailsFromParse();
+                        }
+                    }
+                });
+            }
+        });
     }
 
-    private void signUp(String username, String password, String connection) {
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        ParseFacebookUtils.onActivityResult(requestCode, resultCode, data);
+    }
+
+    void getUserDetailFromFB(){
+         // Suggested by https://disqus.com/by/dominiquecanlas/
+        Bundle parameters = new Bundle();
+        parameters.putString("fields", "email,name");
+
+
+        new GraphRequest(
+                AccessToken.getCurrentAccessToken(),
+                "/me",
+                parameters,
+                HttpMethod.GET,
+                new GraphRequest.Callback() {
+                    public void onCompleted(GraphResponse response) {
+                        try {
+
+                            String email = response.getJSONObject().getString("email");
+                            String name = response.getJSONObject().getString("name");
+                            String password = etPasswordInput.getText().toString();
+                            String connection = atvSchoolName.getText().toString();
+                            etUsernameInput.setText(name);
+                            etEmailInput.setText(email);
+                            signUp(name, password, connection, email);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+        ).executeAsync();
+    }
+
+    private void getUserDetailsFromParse() {
+        ParseUser user = ParseUser.getCurrentUser();
+        etUsernameInput.setText(user.getUsername());
+
+        Toast.makeText(SignupActivity.this, "Welcome back " + etUsernameInput.getText().toString(), Toast.LENGTH_SHORT).show();
+
+    }
+//
+//    void getUserDetailFromParse(){
+//        ParseUser user = ParseUser.getCurrentUser();
+//        etUsernameInput.setText(user.getUsername());
+//        Toast.makeText(getApplicationContext(), "Welcome Back " + etUsernameInput.getText().toString(), Toast.LENGTH_SHORT).show();
+//        //alertDisplayer("Welcome Back","User:"+t_username.getText().toString()+" Login.Email:"+t_email.getText().toString());
+//
+//    }
+
+    private void signUp(String username, String password, String connection, String email) {
         ParseUser user = (ParseUser) ParseUser.create("_User");
         user.setPassword(password);
         user.setUsername(username);
+        user.setEmail(email);
         user.put("connection", connection);
 
         user.signUpInBackground(new SignUpCallback() {
             @Override
             public void done(ParseException e) {
                 if (e == null) {
+                    Toast.makeText(getApplicationContext(), "Welcome " + etUsernameInput.getText().toString(), Toast.LENGTH_SHORT).show();
                     final Intent intent = new Intent(SignupActivity.this, MapActivity.class);
                     startActivity(intent);
                     finish();
