3de35c8dd0cded39e920d8124b5d1103325d5e65
Connie Ye <connieye@fb.com>
2018-08-01 15:10:00 -0700

    splash screen

--- app/src/main/java/green_minds/com/finalproject/activities/MainActivity.java
+++ app/src/main/java/green_minds/com/finalproject/activities/MainActivity.java
@@ -6,34 +6,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
 import green_minds.com.finalproject.R;
 
 public class MainActivity extends AppCompatActivity {
-
-    @BindView(R.id.btn_checkin) public Button btnCheckin;
-    @BindView(R.id.btn_signup) public Button btnSignup;
-    @BindView(R.id.btn_login) public Button btnLogin;
-    @BindView(R.id.btn_map) public Button btnMap;
-    @BindView(R.id.btn_newpin) public Button btnNewpin;
-    @BindView(R.id.btn_userinfo) public Button btnUserinfo;
-    @BindView(R.id.btn_leaderboard) public Button btnLeaderboard;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
-
-        setGoToActivity(btnCheckin, CheckInActivity.class);
-        setGoToActivity(btnSignup, SignupActivity.class);
-        setGoToActivity(btnLogin, LoginActivity.class);
-        setGoToActivity(btnMap, MapActivity.class);
-        setGoToActivity(btnNewpin, NewPinActivity.class);
-        setGoToActivity(btnUserinfo, UserInfoActivity.class);
-        setGoToActivity(btnLeaderboard, LeaderboardActivity.class);
 
     }
 
